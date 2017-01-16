2a96e942116d6b6c6583dc47bfb584081f0b7b38
kaby76 <kenneth.domino@domemtech.com>
2017-01-16 18:27:05 -0500

    Check point: adding in rename, first with highlighting of selected symbol.

---
+++ AntlrVSIX/Rename/HighlightRenameTaggerProvider.cs
@@ -0,0 +1,38 @@
+﻿namespace AntlrVSIX.Rename
+{
+    using System.ComponentModel.Composition;
+    using Microsoft.VisualStudio.Text;
+    using Microsoft.VisualStudio.Text.Editor;
+    using Microsoft.VisualStudio.Text.Operations;
+    using Microsoft.VisualStudio.Text.Tagging;
+    using Microsoft.VisualStudio.Utilities;
+    using Microsoft.VisualStudio.Text.Classification;
+
+    [Export(typeof(IViewTaggerProvider))]
+    [ContentType(Constants.ContentType)]
+    [TagType(typeof(HighlightWordTag))]
+    public class HighlightWordTaggerProvider : IViewTaggerProvider
+    {
+        [Import]
+        IClassifierAggregatorService AggregatorFactory = null;
+
+        [Import]
+        internal ITextSearchService TextSearchService { get; set; }
+
+        [Import]
+        internal ITextStructureNavigatorSelectorService TextStructureNavigatorSelector { get; set; }
+
+        public ITagger<T> CreateTagger<T>(ITextView textView, ITextBuffer buffer) where T : ITag
+        {
+            if (textView.TextBuffer != buffer)
+                return null;
+
+            ITextStructureNavigator textStructureNavigator =
+                TextStructureNavigatorSelector.GetTextStructureNavigator(buffer);
+            
+            IClassifier ag1 = AggregatorFactory.GetClassifier(buffer);
+
+            return new RenameHighlightTagger(textView, buffer, TextSearchService, textStructureNavigator, ag1) as ITagger<T>;
+        }
+    }
+}
---
+++ AntlrVSIX/Rename/RenameHighlightTagger.cs
@@ -0,0 +1,293 @@
+﻿using Antlr4.Runtime;
+using AntlrVSIX.Extensions;
+using AntlrVSIX.Grammar;
+using Microsoft.VisualStudio.Text.Classification;
+
+namespace AntlrVSIX.Rename
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using Microsoft.VisualStudio.Text;
+    using Microsoft.VisualStudio.Text.Editor;
+    using Microsoft.VisualStudio.Text.Operations;
+    using Microsoft.VisualStudio.Text.Tagging;
+
+    public class HighlightWordTag : TextMarkerTag, ITag
+    {
+        public HighlightWordTag() : base("blue") { }
+    }
+
+    public class RenameHighlightTagger : ITagger<HighlightWordTag>
+    {
+        private ITextView View { get; set; }
+        private ITextBuffer SourceBuffer { get; set; }
+        private ITextSearchService TextSearchService { get; set; }
+        private ITextStructureNavigator TextStructureNavigator { get; set; }
+        private object updateLock = new object();
+        private IClassifier _aggregator;
+
+        // The current set of words to highlight
+        private NormalizedSnapshotSpanCollection WordSpans { get; set; }
+        private SnapshotSpan? CurrentWord { get; set; }
+
+        // The current request, from the last cursor movement or view render
+        private SnapshotPoint RequestedPoint { get; set; }
+
+        public RenameHighlightTagger(ITextView view, ITextBuffer sourceBuffer, ITextSearchService textSearchService,
+                                   ITextStructureNavigator textStructureNavigator,
+                                   IClassifier aggregator)
+        {
+            View = view;
+            SourceBuffer = sourceBuffer;
+            TextSearchService = textSearchService;
+            TextStructureNavigator = textStructureNavigator;
+            _aggregator = aggregator;
+
+            WordSpans = new NormalizedSnapshotSpanCollection();
+            CurrentWord = null;
+
+            // Subscribe to both change events in the view - any time the view is updated
+            // or the caret is moved, we refresh our list of highlighted words.
+            View.Caret.PositionChanged += CaretPositionChanged;
+            View.LayoutChanged += ViewLayoutChanged;
+        }
+
+        private void ViewLayoutChanged(object sender, TextViewLayoutChangedEventArgs e)
+        {
+            // If a new snapshot wasn't generated, then skip this layout
+            if (e.NewViewState.EditSnapshot != e.OldViewState.EditSnapshot)
+            {
+                UpdateAtCaretPosition(View.Caret.Position);
+            }
+        }
+
+        private void CaretPositionChanged(object sender, CaretPositionChangedEventArgs e)
+        {
+            UpdateAtCaretPosition(e.NewPosition);
+        }
+
+        private void UpdateAtCaretPosition(CaretPosition caretPoisition)
+        {
+            SnapshotPoint? point = caretPoisition.Point.GetPoint(SourceBuffer, caretPoisition.Affinity);
+
+            if (!point.HasValue)
+                return;
+
+            // If the new cursor position is still within the current word (and on the same snapshot),
+            // we don't need to check it.
+            if (CurrentWord.HasValue &&
+                CurrentWord.Value.Snapshot == View.TextSnapshot &&
+                point.Value >= CurrentWord.Value.Start &&
+                point.Value <= CurrentWord.Value.End)
+            {
+                return;
+            }
+
+            RequestedPoint = point.Value;
+
+            ThreadPool.QueueUserWorkItem(UpdateWordAdornments);
+        }
+
+        private void UpdateWordAdornments(object threadContext)
+        {
+            SnapshotPoint currentRequest = RequestedPoint;
+
+            List<SnapshotSpan> wordSpans = new List<SnapshotSpan>();
+
+            // Find all words in the buffer like the one the caret is on
+            TextExtent word = TextStructureNavigator.GetExtentOfWord(currentRequest);
+
+            bool foundWord = true;
+
+            // If we've selected something not worth highlighting, we might have
+            // missed a "word" by a little bit
+            if (!WordExtentIsValid(currentRequest, word))
+            {
+                // Before we retry, make sure it is worthwhile
+                if (word.Span.Start != currentRequest ||
+                    currentRequest == currentRequest.GetContainingLine().Start ||
+                    char.IsWhiteSpace((currentRequest - 1).GetChar()))
+                {
+                    foundWord = false;
+                }
+                else
+                {
+                    // Try again, one character previous.  If the caret is at the end of a word, then
+                    // this will pick up the word we are at the end of.
+                    word = TextStructureNavigator.GetExtentOfWord(currentRequest - 1);
+
+                    // If we still aren't valid the second time around, we're done
+                    if (!WordExtentIsValid(currentRequest, word))
+                        foundWord = false;
+                }
+            }
+
+            if (!foundWord)
+            {
+                // If we couldn't find a word, just clear out the existing markers
+                SynchronousUpdate(currentRequest, new NormalizedSnapshotSpanCollection(), null);
+                return;
+            }
+
+            SnapshotSpan currentWord = word.Span;
+
+            // If this is the same word we currently have, we're done (e.g. caret moved within a word).
+            if (CurrentWord.HasValue && currentWord == CurrentWord)
+                return;
+
+            /* Find spans using simple text search....
+            FindData findData = new FindData(currentWord.GetText(), currentWord.Snapshot);
+            findData.FindOptions = FindOptions.WholeWord | FindOptions.MatchCase;
+            wordSpans.AddRange(TextSearchService.FindAll(findData));
+            */
+
+            // Verify current word is a grammar symbol.
+            //  Now, check for valid classification type.
+            string classification = null;
+            ClassificationSpan[] c1 = _aggregator.GetClassificationSpans(currentWord).ToArray();
+            foreach (ClassificationSpan cl in c1)
+            {
+                classification = cl.ClassificationType.Classification.ToLower();
+                if (classification == AntlrVSIX.Constants.ClassificationNameTerminal)
+                {
+                    break;
+                }
+                else if (classification == AntlrVSIX.Constants.ClassificationNameNonterminal)
+                {
+                    break;
+                }
+            }
+            if (classification == null) return;
+
+            SnapshotSpan span = currentWord;
+            ITextView view = this.View;
+
+            // First, find out what this view is, and what the file is.
+            ITextBuffer buffer = view.TextBuffer;
+            ITextDocument doc = buffer.GetTextDocument();
+            string path = doc.FilePath;
+
+            List<IToken> where = new List<IToken>();
+            List<ParserDetails> where_details = new List<ParserDetails>();
+            IToken token = null;
+            foreach (var kvp in ParserDetails._per_file_parser_details)
+            {
+                string file_name = kvp.Key;
+                ParserDetails details = kvp.Value;
+                if (classification == AntlrVSIX.Constants.ClassificationNameNonterminal)
+                {
+                    var it = details._ant_nonterminals.Where(
+                        (t) => t.Text == span.GetText());
+                    where.AddRange(it);
+                    foreach (var i in it) where_details.Add(details);
+                }
+                else if (classification == AntlrVSIX.Constants.ClassificationNameTerminal)
+                {
+                    var it = details._ant_terminals.Where(
+                        (t) => t.Text == span.GetText());
+                    where.AddRange(it);
+                    foreach (var i in it) where_details.Add(details);
+                }
+                else if (classification == AntlrVSIX.Constants.ClassificationNameLiteral)
+                {
+                    var it = details._ant_literals.Where(
+                        (t) => t.Text == span.GetText());
+                    where.AddRange(it);
+                    foreach (var i in it) where_details.Add(details);
+                }
+            }
+            if (!where.Any()) return;
+
+            // Populate the Antlr find results model/window with file/line/col info
+            // for each occurrence.
+            FindAntlrSymbolsModel.Instance.Results = new System.Collections.ObjectModel.ObservableCollection<Entry>();
+            for (int i = 0; i < where.Count; ++i)
+            {
+                IToken x = where[i];
+                ParserDetails y = where_details[i];
+                var w = new Entry() { FileName = y.full_file_name, LineNumber = x.Line, ColumnNumber = x.Column, Token = x };
+                FindAntlrSymbolsModel.Instance.Results.Add(w);
+            }
+
+            // Now, for all entries which are in this buffer, highlight.
+            //wordSpans.Add(currentWord);
+            for (int i = 0; i < FindAntlrSymbolsModel.Instance.Results.Count; ++i)
+            {
+                var w = FindAntlrSymbolsModel.Instance.Results[i];
+                if (w.FileName == path)
+                {
+                    // Create new span in the appropriate view.
+                    ITextSnapshot cc = buffer.CurrentSnapshot;
+                    SnapshotSpan ss = new SnapshotSpan(cc, w.Token.StartIndex, 1 + w.Token.StopIndex - w.Token.StartIndex);
+                    SnapshotPoint sp = ss.Start;
+                    wordSpans.Add(ss);
+                }
+            }
+
+            // If we are still up-to-date (another change hasn't happened yet), do a real update
+            if (currentRequest == RequestedPoint)
+                SynchronousUpdate(currentRequest, new NormalizedSnapshotSpanCollection(wordSpans), currentWord);
+        }
+
+        static bool WordExtentIsValid(SnapshotPoint currentRequest, TextExtent word)
+        {
+            return word.IsSignificant && currentRequest.Snapshot.GetText(word.Span).Any(c => char.IsLetter(c));
+        }
+
+        private void SynchronousUpdate(SnapshotPoint currentRequest, NormalizedSnapshotSpanCollection newSpans, SnapshotSpan? newCurrentWord)
+        {
+            lock (updateLock)
+            {
+                if (currentRequest != RequestedPoint)
+                    return;
+
+                WordSpans = newSpans;
+                CurrentWord = newCurrentWord;
+
+                var tempEvent = TagsChanged;
+                if (tempEvent != null)
+                    tempEvent(this, new SnapshotSpanEventArgs(new SnapshotSpan(SourceBuffer.CurrentSnapshot, 0, SourceBuffer.CurrentSnapshot.Length)));
+            }
+        }
+
+        public IEnumerable<ITagSpan<HighlightWordTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+            if (CurrentWord == null)
+                yield break;
+
+            // Hold on to a "snapshot" of the word spans and current word, so that we maintain the same
+            // collection throughout
+            SnapshotSpan currentWord = CurrentWord.Value;
+            NormalizedSnapshotSpanCollection wordSpans = WordSpans;
+
+            if (spans.Count == 0 || WordSpans.Count == 0)
+                yield break;
+
+            // If the requested snapshot isn't the same as the one our words are on, translate our spans
+            // to the expected snapshot
+            if (spans[0].Snapshot != wordSpans[0].Snapshot)
+            {
+                wordSpans = new NormalizedSnapshotSpanCollection(
+                    wordSpans.Select(span => span.TranslateTo(spans[0].Snapshot, SpanTrackingMode.EdgeExclusive)));
+
+                currentWord = currentWord.TranslateTo(spans[0].Snapshot, SpanTrackingMode.EdgeExclusive);
+            }
+
+            // First, yield back the word the cursor is under (if it overlaps)
+            // Note that we'll yield back the same word again in the wordspans collection;
+            // the duplication here is expected.
+            if (spans.OverlapsWith(new NormalizedSnapshotSpanCollection(currentWord)))
+                yield return new TagSpan<HighlightWordTag>(currentWord, new HighlightWordTag());
+
+            // Second, yield all the other words in the file
+            foreach (SnapshotSpan span in NormalizedSnapshotSpanCollection.Overlap(spans, wordSpans))
+            {
+                yield return new TagSpan<HighlightWordTag>(span, new HighlightWordTag());
+            }
+        }
+
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
+    }
+}
