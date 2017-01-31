6d53b1a280842fb3091ea00693eb8932cc1cbbcc
kaby76 <kenneth.domino@domemtech.com>
2017-01-31 10:42:16 -0500

    Adding stand-alone parser for Antlr4 grammars. Use this tool to check whether all the files in the corpus for codebuff parse. Note, the grammar used in codebuff is not the same as what this tool used before. So there may be regressions.

---
+++ AntlrParserTool/AntlrParserTool/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+
+namespace AntlrParserTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommonTokenStream cts = new CommonTokenStream(
+                new ANTLRv4Lexer(
+                    new AntlrInputStream(
+                        new StreamReader(
+                            args[0]).ReadToEnd())));
+            var ant_parser = new ANTLRv4Parser(cts);
+            ant_parser.grammarSpec();
+        }
+    }
+}
