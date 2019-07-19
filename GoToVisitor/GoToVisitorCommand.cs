e1decbe3b5d57a78e7907deeb6b3b3999aadeba0
Ken Domino <kenneth.domino@domemtech.com>
2019-07-19 13:34:02 -0400

    Adding in Roslyn example code for go to visitor/listener.

--- GoToVisitor/GoToVisitorCommand.cs
+++ GoToVisitor/GoToVisitorCommand.cs
@@ -3,10 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace AntlrVSIX.GoToVisitor
 {
-    class GoToVisitorCommand
+    public class GoToVisitorCommand
     {
+        public GoToVisitorCommand()
+        {
+        }
+
+        private void Compile()
+        {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(
+                @"using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+ 
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(""Hello, World!"");
+        }
+    }
+}");
+
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+
+            var firstMember = root.Members[0];
+
+            var helloWorldDeclaration = (NamespaceDeclarationSyntax)firstMember;
+
+            var programDeclaration = (ClassDeclarationSyntax)helloWorldDeclaration.Members[0];
+
+            var mainDeclaration = (MethodDeclarationSyntax)programDeclaration.Members[0];
+
+            var argsParameter = mainDeclaration.ParameterList.Parameters[0];
+
+            var firstParameters = root.DescendantNodes()
+                .OfType<MethodDeclarationSyntax>()
+                .Where(methodDeclaration => methodDeclaration.Identifier.ValueText == "Main")
+                .Select(methodDeclaration => methodDeclaration.ParameterList.Parameters.First());
+
+            var argsParameter2 = firstParameters.Single();
+
+        }
     }
 }
