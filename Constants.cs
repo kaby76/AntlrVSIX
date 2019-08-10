7270b6fb74c8d2d4ab72c8c95206262c580cc3c9
Ken Domino <kenneth.domino@domemtech.com>
2019-08-10 08:21:42 -0400

    Bump and release.

--- Constants.cs
+++ Constants.cs
@@ -10,7 +10,7 @@
          * be set in the manifest file.
          * Make sure to change the version in source.extension.vsixmanifest as well!
          */
-        public const string Version = "2.0.3";
+        public const string Version = "2.0.4";
 
         /* Invariants of the AntlrVSIX extension.
          */
