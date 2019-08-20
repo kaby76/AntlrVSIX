0798b2a767ad18da6c7516ec7db3a7f012e7abe9
Ken Domino <kenneth.domino@domemtech.com>
2019-08-19 20:56:47 -0400

    Fix about box.

--- About/AboutCommand.cs
+++ About/AboutCommand.cs
@@ -9,6 +9,7 @@
     {
         private readonly Microsoft.VisualStudio.Shell.Package _package;
         private MenuCommand _menu_item1;
+        private MenuCommand _menu_item2;
 
         private AboutCommand(Microsoft.VisualStudio.Shell.Package package)
         {
@@ -27,11 +28,19 @@
 
             {
                 // Set up hook for context menu.
-                var menuCommandID = new CommandID(new Guid(AntlrVSIX.Constants.guidMenuAndCommandsCmdSet), 0x7010);
+                var menuCommandID = new CommandID(new Guid(AntlrVSIX.Constants.guidVSPackageCommandCodeWindowContextMenuCmdSet), 0x7010);
                 _menu_item1 = new MenuCommand(this.MenuItemCallback, menuCommandID);
                 _menu_item1.Enabled = true;
                 _menu_item1.Visible = true;
                 commandService.AddCommand(_menu_item1);
+            }
+            {
+                // Set up hook for context menu.
+                var menuCommandID = new CommandID(new Guid(AntlrVSIX.Constants.guidMenuAndCommandsCmdSet), 0x7010);
+                _menu_item2 = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                _menu_item2.Enabled = true;
+                _menu_item2.Visible = true;
+                commandService.AddCommand(_menu_item2);
             }
         }
         public static AboutCommand Instance { get; private set; }
