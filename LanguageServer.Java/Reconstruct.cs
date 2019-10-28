cdcd314810d6cfff9af69627011c90679edce1b1
Ken Domino <kenneth.domino@domemtech.com>
2019-10-28 08:04:23 -0400

    Adding in tool tip code for Java.

---
+++ LanguageServer.Java/Reconstruct.cs
@@ -0,0 +1,24 @@
+﻿namespace LanguageServer.Java
+{
+    using Antlr4.Runtime.Tree;
+    using System.Text;
+
+    class Reconstruct
+    {
+        public static void Doit(StringBuilder sb, IParseTree node)
+        {
+            if (node is TerminalNodeImpl)
+            {
+                sb.Append(" " + node.GetText());
+            }
+            else
+            {
+                for (int i = 0; i < node.ChildCount; ++i)
+                {
+                    var c = node.GetChild(i);
+                    Doit(sb, c);
+                }
+            }
+        }
+    }
+}
--- test/Dog.java
+++ test/Dog.java
@@ -10,48 +10,48 @@ public class Dog
     public Dog(String name, String breed, 
 	       int age, String color) 
     { 
-	this.name = name; 
-	this.breed = breed; 
-	this.age = age; 
-	this.color = color; 
+		this.name = name; 
+		this.breed = breed; 
+		this.age = age; 
+		this.color = color; 
     } 
 
     // method 1 
     public String getName() 
     { 
-	return name; 
+		return name; 
     } 
 
     // method 2 
     public String getBreed() 
     { 
-	return breed; 
+		return breed; 
     } 
 
     // method 3 
     public int getAge() 
     { 
-	return age; 
+		return age; 
     } 
 
     // method 4 
     public String getColor() 
     { 
-	return color; 
+		return color; 
     } 
 
     @Override
     public String toString() 
     { 
-	return("Hi my name is "+ this.getName()+ 
-	       ".\nMy breed,age and color are " + 
-	       this.getBreed()+"," + this.getAge()+ 
-	       ","+ this.getColor()); 
+		return("Hi my name is "+ this.getName()+ 
+			   ".\nMy breed,age and color are " + 
+			   this.getBreed()+"," + this.getAge()+ 
+			   ","+ this.getColor()); 
     } 
 
     public static void main(String[] args) 
     { 
-	Dog tuffy = new Dog("tuffy","papillon", 5, "white"); 
-	System.out.println(tuffy.toString()); 
+		Dog tuffy = new Dog("tuffy","papillon", 5, "white"); 
+		System.out.println(tuffy.toString()); 
     } 
 }
