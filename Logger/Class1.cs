36e4547e718779379613511f4e083c8ddcdfe52d
Ken Domino <kenneth.domino@domemtech.com>
2019-11-01 20:12:43 -0400

    Commenting out dialog box for now until a db server is up.

--- Logger/Class1.cs
+++ Logger/Class1.cs
@@ -14,19 +14,19 @@
             var options = Basics.XOptions.GetProperty("OptInLogging");
             if (options == "0")
             {
-                DialogResult result = MessageBox.Show("AntlrVSIX would like to send a stack trace of an exception. Can we do that? (You can change this in the AntlrVSIX options at any time.)",
-                    "Warning",
-                    MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
-                if (result == DialogResult.Yes)
-                {
-                    //code for Yes
-                    Basics.XOptions.SetProperty("OptInLogging", "1");
-                }
-                else if (result == DialogResult.No)
-                {
-                    //code for No
-                    Basics.XOptions.SetProperty("OptInLogging", "2");
-                }
+                //DialogResult result = MessageBox.Show("AntlrVSIX would like to send a stack trace of an exception. Can we do that? (You can change this in the AntlrVSIX options at any time.)",
+                //    "Warning",
+                //    MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                //if (result == DialogResult.Yes)
+                //{
+                //    //code for Yes
+                //    Basics.XOptions.SetProperty("OptInLogging", "1");
+                //}
+                //else if (result == DialogResult.No)
+                //{
+                //    //code for No
+                //    Basics.XOptions.SetProperty("OptInLogging", "2");
+                //}
             }
             else if (options == "1")
             {
