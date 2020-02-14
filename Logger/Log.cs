ad0843536f729f3601fa737f4f46692de8690041
Ken Domino <kenneth.domino@domemtech.com>
2020-02-14 10:22:52 -0500

    Add in logging.

---
+++ Logger/Log.cs
@@ -0,0 +1,24 @@
+﻿namespace Logger
+{
+    using System;
+    using System.Net.Http;
+
+    public class Log
+    {
+        public static void Notify(string message)
+        {
+            var options = Options.Option.GetInt32("OptInLogging");
+            if (options == 1)
+            {
+                try
+                {
+                    HttpClient httpClient = new HttpClient();
+                    var result = httpClient.GetAsync("http://domemtech.com/home/db?" + message).Result;
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+    }
+}
