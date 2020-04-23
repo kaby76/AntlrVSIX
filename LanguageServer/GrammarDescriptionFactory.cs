b97d4ca5a10855174dd0699a88ee107869431780
Ken Domino <kenneth.domino@domemtech.com>
2020-04-23 06:40:21 -0400

    Clean up---cannot utilize dynamic loading of dll in VS.

--- LanguageServer/GrammarDescriptionFactory.cs
+++ LanguageServer/GrammarDescriptionFactory.cs
@@ -5,55 +5,6 @@
     using System.Collections.Generic;
     using System.Reflection;
 
-    public class ManualAssemblyResolver : IDisposable
-    {
-
-        private readonly List<Assembly> _assemblies;
-
-        public ManualAssemblyResolver()
-        {
-            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(OnAssemblyResolve);
-            _assemblies = new List<Assembly>();
-        }
-
-        public void Add(Assembly assembly)
-        {
-            _assemblies.Add(assembly);
-        }
-
-        public ManualAssemblyResolver(Assembly assembly)
-        {
-            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(OnAssemblyResolve);
-            if (assembly == null)
-            {
-                throw new ArgumentNullException("assembly");
-            }
-
-            _assemblies = new List<Assembly>
-            {
-                assembly
-            };
-        }
-
-        public void Dispose()
-        {
-            AppDomain.CurrentDomain.AssemblyResolve -= OnAssemblyResolve;
-        }
-
-        private Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
-        {
-            foreach (Assembly assembly in _assemblies)
-            {
-                if (args.Name == assembly.FullName)
-                {
-                    return assembly;
-                }
-            }
-
-            return null;
-        }
-    }
-
     public class GrammarDescriptionFactory
     {
         private static readonly IGrammarDescription _antlr = new AntlrGrammarDescription();
