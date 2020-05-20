cb9c60faf78476d217d0829c04b48b6e54617ab7
Ken Domino <kenneth.domino@domemtech.com>
2020-05-20 17:14:38 -0400

    Really clean if --all set.

--- clean.sh
+++ clean.sh
@@ -1,7 +1,19 @@
-#
+#! /bin/sh -
+
 rm -rf */obj
 rm -rf */bin
 rm -rf obj bin
 rm -rf .vs
 rm -rf packages
 rm -rf "$LOCALAPPDATA/Microsoft/VisualStudio/16.0"*"Exp/Extensions"
+
+while test $# -gt 0
+do
+    case "$1" in
+        --all)
+	    rm -rf "$LOCALAPPDATA/Microsoft/VisualStudio/16.0"*"Exp"
+            ;;
+    esac
+    shift
+done
+
