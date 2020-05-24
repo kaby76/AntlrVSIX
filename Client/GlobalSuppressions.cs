af3e94e0264e7290ae4c35e09234948766420df6
Ken Domino <kenneth.domino@domemtech.com>
2020-05-24 05:19:08 -0400

    Clean up.

---
+++ Client/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>", Scope = "member", Target = "~M:LspAntlr.AntlrLanguageClient.CMGotoListener(System.String,System.Boolean,System.Int32)~LanguageServer.CMGotoResult")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>", Scope = "member", Target = "~M:LspAntlr.AntlrLanguageClient.CMReorderParserRules(System.String,System.Int32,LspAntlr.ReorderType)")]
