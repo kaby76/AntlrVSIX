6c17a89fee1e98d074c32538a41e8b445908349e
Ken Domino <kenneth.domino@domemtech.com>
2020-05-24 04:02:06 -0400

    Clean and simpliify.

--- Client/Menus/EliminateAntlrKeywordsInRules.cs
+++ Client/Menus/EliminateAntlrKeywordsInRules.cs
@@ -12,8 +12,6 @@
     {
         private readonly AntlrLanguageClient _package;
         private readonly MenuCommand _menu_item1;
-        private string current_grammar_ffn;
-
 
         private EliminateAntlrKeywordsInRules(AntlrLanguageClient package)
         {
@@ -63,9 +61,7 @@
             Instance = new EliminateAntlrKeywordsInRules(package);
         }
 
-#pragma warning disable VSTHRD100
-        private async void MenuItemCallback(object sender, EventArgs e)
-#pragma warning restore VSTHRD100
+        private void MenuItemCallback(object sender, EventArgs e)
         {
             try
             {
@@ -74,39 +70,17 @@
                 ////////////////////////
 
                 IVsTextManager manager = ((IServiceProvider)ServiceProvider).GetService(typeof(VsTextManagerClass)) as IVsTextManager;
-                if (manager == null)
-                {
-                    return;
-                }
-
+                if (manager == null) return;
                 manager.GetActiveView(1, null, out IVsTextView view);
-                if (view == null)
-                {
-                    return;
-                }
-
+                if (view == null) return;
                 view.GetCaretPos(out int l, out int c);
                 view.GetBuffer(out IVsTextLines buf);
-                if (buf == null)
-                {
-                    return;
-                }
-
-                IWpfTextView xxx = AntlrLanguageClient.AdaptersFactory.GetWpfTextView(view);
-                ITextBuffer buffer = xxx.TextBuffer;
-                string ffn = buffer.GetFFN();
-                if (ffn == null)
-                {
-                    return;
-                }
-                current_grammar_ffn = ffn;
-
+                if (buf == null) return;
+                ITextBuffer buffer = AntlrLanguageClient.AdaptersFactory.GetWpfTextView(view)?.TextBuffer;
+                string ffn = buffer?.GetFFN();
+                if (ffn == null) return;
                 Workspaces.Document document = Workspaces.Workspace.Instance.FindDocument(ffn);
-                if (document == null)
-                {
-                    return;
-                }
-
+                if (document == null) return;
                 int pos = LanguageServer.Module.GetIndex(l, c, document);
                 AntlrLanguageClient.CMEliminateAntlrKeywordsInRules(ffn);
             }
