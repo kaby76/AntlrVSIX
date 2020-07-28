48613b260a38f056590b28e1e0b25cecfc257015
Ken Domino <kenneth.domino@domemtech.com>
2020-07-28 17:15:44 -0400

    Moving AntlrTreeEditing into a nuget package.

--- AntlrTreeEditing/AntlrDOM/AntlrText.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AntlrTreeEditing.AntlrDOM
-{
-    using org.w3c.dom;
-
-    public class AntlrText : AntlrNode, Text
-    {
-        public string Data { get; set; }
-    }
-}
--- AntlrTreeEditing/clean.sh
+++
@@ -1,5 +0,0 @@
-#
-
-rm -f o
-rm -rf bin obj
-rm -rf /c/Users/kenne/.nuget/packages/antlr4buildtasks
