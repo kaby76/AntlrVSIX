e420abc898c6dd9d24ae7b39c0e90ee3d0f1ee84
Ken Domino <kenneth.domino@domemtech.com>
2020-10-18 09:09:38 -0400

    Additional fix for Performance Problems #96. Roll forward to updated LSP types API.

--- Workspaces/Util.cs
+++ Workspaces/Util.cs
@@ -1,9 +1,14 @@
-﻿namespace Workspaces
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Workspaces
 {
     using System.IO;
 
     public class Util
     {
+        private static Dictionary<string, string> cache = new Dictionary<string, string>();
+
         public static string GetProperDirectoryCapitalization(DirectoryInfo dirInfo)
         {
             DirectoryInfo parentDirInfo = dirInfo.Parent;
@@ -18,12 +23,24 @@
 
         public static string GetProperFilePathCapitalization(string filename)
         {
+            if (cache.TryGetValue(filename, out string new_filename))
+                return new_filename;
             FileInfo fileInfo = new FileInfo(filename);
-            if (!fileInfo.Exists) return filename;
+            if (!fileInfo.Exists)
+            {
+                cache[filename] = filename;
+                return filename;
+            }
             DirectoryInfo dirInfo = fileInfo.Directory;
-            if (dirInfo.GetFiles(fileInfo.Name).Length == 0) return filename;
-            return Path.Combine(GetProperDirectoryCapitalization(dirInfo),
+            if (dirInfo.GetFiles(fileInfo.Name).Length == 0)
+            {
+                cache[filename] = filename;
+                return filename;
+            }
+            string result = Path.Combine(GetProperDirectoryCapitalization(dirInfo),
                             dirInfo.GetFiles(fileInfo.Name)[0].Name);
+            cache[filename] = result;
+            return result;
         }
     }
 }
