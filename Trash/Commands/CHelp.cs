1f72cd93dd91e59c92b2b5d1fa87801de519d485
Ken Domino <kenneth.domino@domemtech.com>
2020-11-04 07:29:53 -0500

    Fix "help" command with new commands.

--- Trash/Commands/CHelp.cs
+++ Trash/Commands/CHelp.cs
@@ -24,7 +24,9 @@ analyze - perform an analysis of a grammar.
 !int - repeat a previous command.
 cd - change directory.
 combine - create a combined grammar from the lexer and parser grammars on the top of stack.
+delabel - remove all labels from an Antlr4 grammar.
 delete - delete nodes in the parse tree for a grammar and reform the grammar.
+. (aka 'dot') - print out the parse tree for the file at the top of stack.
 find - find nodes in the parse tree for a grammar.
 fold - replace a sequence of symbols on the RHS of a rule with a LHS symbol of another rule.
 foldlit - replace a literal on the RHS of a rule with a LHS symbol of another rule.
@@ -47,12 +49,16 @@ rotate - rotate the stack.
 rr - replace left recursion with right recursion.
 run - generate a parser, compile it, and run it on input.
 rup - remove useless parentheses in a grammar rule.
+set - set a value.
 split - split a combined grammar.
+st - print the tree using 'toStringTree()'.
 stack - print the stack.
+text - print the tree source.
 ulliteral - convert a lexer rule for a simple string literal to accept a string in any case.
 unalias - unalias a command.
 unfold - unfold a grammar rule symbol.
 ungroup - ungroup a parenthesized alt.
+unulliteral - inverse transform of ulliteral.
 version - print out the version of Trash.
 workspace - create a new workspace for the run command.
 write - write a file to disk.
@@ -70,8 +76,12 @@ write - write a file to disk.
                     new CCd().Help();
                 else if (cmd == "combine")
                     new CCombine().Help();
+                else if (cmd == "delabel")
+                    new CDelabel().Help();
                 else if (cmd == "delete")
                     new CDelete().Help();
+                else if (cmd == "dot")
+                    new CDot().Help();
                 else if (cmd == "find")
                     new CFind().Help();
                 else if (cmd == "fold")
@@ -114,10 +124,16 @@ write - write a file to disk.
                     new CRun().Help();
                 else if (cmd == "rup")
                     new CRup().Help();
+                else if (cmd == "set")
+                    new CSet().Help();
                 else if (cmd == "split")
                     new CSplit().Help();
+                else if (cmd == "st")
+                    new CSt().Help();
                 else if (cmd == "stack")
                     new CStack().Help();
+                else if (cmd == "text")
+                    new CText().Help();
                 else if (cmd == "ulliteral")
                     new CUlliteral().Help();
                 else if (cmd == "unalias")
@@ -126,6 +142,10 @@ write - write a file to disk.
                     new CUnfold().Help();
                 else if (cmd == "ungroup")
                     new CUngroup().Help();
+                else if (cmd == "unulliteral")
+                    new CUnulliteral().Help();
+                else if (cmd == "version")
+                    new CVersion().Help();
                 else if (cmd == "workspace")
                     new CWorkspace().Help();
                 else if (cmd == "write")
