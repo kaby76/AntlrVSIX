6e7c824afe1d5adc1b358c97a65dc9f84b00159d
Ken Domino <kenneth.domino@domemtech.com>
2020-11-05 06:23:08 -0500

    Add in install vsce.

--- VsCode/install-cmd.bat
+++ VsCode/install-cmd.bat
@@ -4,7 +4,7 @@ call npm i vscode-languageclient@7.0.0-next.9
 call npm i vscode-languageserver@7.0.0-next.7
 call npm i vscode-languageserver-protocol@3.16.0-next.7
 call npm i vscode-languageserver-types@3.16.0-next.3
-
+call npm install -g vsce
 echo d | xcopy /E /H ..\Server\bin\Debug\netcoreapp3.1 server\netcoreapp3.1
 echo d | xcopy /E /H ..\Trash\bin\Debug\netcoreapp3.1 Trash
 call npm install
