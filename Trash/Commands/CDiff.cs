e9eb43247d306c4900843a10a518c4a603c54d25
Ken Domino <kenneth.domino@domemtech.com>
2020-12-15 14:47:30 -0500

    Changing output of diff.

--- Trash/Commands/CDiff.cs
+++ Trash/Commands/CDiff.cs
@@ -41,30 +41,57 @@ Example:
             var result = ZhangShashaCSharp.Tree.ZhangShasha(trees[0], trees[1]);
             foreach (var o in result.Item2)
             {
-                System.Console.Write(o.O.ToString() + " " + o.N1 + " => " + o.N2 + " ");
+//                System.Console.Write(o.O.ToString() + " " + o.N1 + " => " + o.N2 + " ");
                 if (o.O == Operation.Op.Insert)
                 {
                     var zs_node = trees[1].all_nodes[o.N2];
-                    System.Console.Write(zs_node.label);
+//                    System.Console.Write(zs_node.label);
                     var a_node = trees[1].antlr_nodes[zs_node];
                     if (a_node is TerminalNodeImpl term)
                     {
                         var name = term.Symbol.Text;
-                        System.Console.Write(" " + name);
+                        System.Console.WriteLine("+++ " + name);
                     }
                 }
-                System.Console.WriteLine();
+                else if (o.O == Operation.Op.Delete)
+                {
+                    var zs_node = trees[0].all_nodes[o.N1];
+                    //                    System.Console.Write(zs_node.label);
+                    var a_node = trees[0].antlr_nodes[zs_node];
+                    if (a_node is TerminalNodeImpl term)
+                    {
+                        var name = term.Symbol.Text;
+                        System.Console.WriteLine("--- " + name);
+                    }
+                }
+                else if (o.O == Operation.Op.Change)
+                {
+                    var zs_node = trees[0].all_nodes[o.N1];
+                    var a_node = trees[0].antlr_nodes[zs_node];
+                    var zs_node2 = trees[1].all_nodes[o.N2];
+                    var a_node2 = trees[1].antlr_nodes[zs_node2];
+                    if (a_node is TerminalNodeImpl term)
+                    {
+                        var name = term.Symbol.Text;
+                        System.Console.WriteLine("--- " + name);
+                    }
+                    if (a_node2 is TerminalNodeImpl term2)
+                    {
+                        var name = term2.Symbol.Text;
+                        System.Console.WriteLine("+++ " + name);
+                    }
+                }
             }
-            System.Console.WriteLine("======");
-            foreach (var o in trees[0].all_nodes)
-            {
-                System.Console.WriteLine(o.Key + " " + o.Value.label + " " + trees[0].antlr_nodes[o.Value]);
-            }
-            System.Console.WriteLine("======");
-            foreach (var o in trees[1].all_nodes)
-            {
-                System.Console.WriteLine(o.Key + " " + o.Value.label + " " + trees[1].antlr_nodes[o.Value]);
-            }
+            //System.Console.WriteLine("======");
+            //foreach (var o in trees[0].all_nodes)
+            //{
+            //    System.Console.WriteLine(o.Key + " " + o.Value.label + " " + trees[0].antlr_nodes[o.Value]);
+            //}
+            //System.Console.WriteLine("======");
+            //foreach (var o in trees[1].all_nodes)
+            //{
+            //    System.Console.WriteLine(o.Key + " " + o.Value.label + " " + trees[1].antlr_nodes[o.Value]);
+            //}
         }
     }
 }
